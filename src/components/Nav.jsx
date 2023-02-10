7e37f852a8189c157545f6cb7424d7a579491370
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2023-02-10 08:10:14 +0100

    feat:loyalbaze

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Image from "next/image";
+
+function Nav() {
+  return (
+    <nav>
+      <div className="nav-items">
+        <p className="page-title">
+          <p className="l">l</p>
+          <Image src="/images/tittle-love.svg" width={30} height={40} />{" "}
+          <p className="baze">yalbaze</p>
+        </p>
+        <div className="request-div">
+          <p className="request">Request a call</p>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
